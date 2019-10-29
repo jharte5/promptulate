84ac22fc028e5d1dc8eb5fc571019feb69a2754c
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-29 14:12:50 -0400

    ugly but works

--- calc-you-later.js
+++ calc-you-later.js
@@ -1,1 +1,14 @@
-const result = calculate(operation, num1, num2);
+const result = window.prompt('Please, type in some numbers!')
+const arrNum = result.split(' ')
+operation = result[0]  
+num1 = Number(arrNum[1])
+num2 = Number(arrNum[2])
+
+const resolution = calculate(operation, num1, num2);
+ 
+console.log(resolution)
+const printAnswer = document.querySelector('#result');
+printAnswer.innerText = resolution
+// printAnswer.appendChild(result);
+
+
