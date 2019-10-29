2b08cb247e54d11812840db5e8c3b79cd424b9e7
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-29 15:01:21 -0400

    added comments

--- calc-you-later.js
+++ calc-you-later.js
@@ -3,12 +3,14 @@ const arrNum = result.split(' ')
 operation = result[0]  
 num1 = Number(arrNum[1])
 num2 = Number(arrNum[2])
-
+// Array Deconstruction
+// const [operation, num1, num2] = result
 const resolution = calculate(operation, num1, num2);
  
 console.log(resolution)
 const printAnswer = document.querySelector('#result');
 printAnswer.innerText = resolution
-// printAnswer.appendChild(result);
+
+//document.querySelector('#result').innerText = calculate(operation, num1, num2)
 
 
